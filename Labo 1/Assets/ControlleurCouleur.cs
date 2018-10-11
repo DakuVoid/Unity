30ae005fb881f5f6b51ba70392759757702b2f6a
Félix A. Gravel <pure_x_own3r_qc@hotmail.com>
2018-10-11 13:51:15 -0400

    lol wtf

---
+++ Labo 1/Assets/ControlleurCouleur.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Renderer))]
+public class ControlleurCouleur : MonoBehaviour
+{
+    Renderer ImageAChanger { get; set; }
+
+	void Start ()
+    {
+        ImageAChanger = GetComponent<Renderer>();   		
+	}
+
+    public void ChangerCouleur(Color nouvelleCouleur)
+    {
+        ImageAChanger.material.color = nouvelleCouleur;
+    }
+}
